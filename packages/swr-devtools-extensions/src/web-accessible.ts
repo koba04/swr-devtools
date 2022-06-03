9d3753899c012cac1145df6e2a2d2f23b454dcef
Toru Kobayashi <koba0004@gmail.com>
2022-06-04 00:58:41 +0900

    wip: sent post messages in a middleware

--- packages/swr-devtools-extensions/src/web-accessible.ts
+++ packages/swr-devtools-extensions/src/web-accessible.ts
@@ -2,8 +2,23 @@ const middleware: any =
   (useSWRNext: any) =>
   (...args: any[]) => {
     const result = useSWRNext(...args);
-    console.log("Inpected!!!!", result);
+    console.log("inspected", { result, args });
+    const [key] = args;
+    const { data, error, isLoading, isValidating } = result;
+    window.postMessage({
+      type: "updated_swr_cache",
+      payload: {
+        key,
+        value: {
+          data,
+          error,
+          isLoading,
+          isValidating,
+        },
+      },
+    });
     return result;
   };
+
 // @ts-expect-error
 window.__SWR_DEVTOOLS_USE__ = [middleware];
