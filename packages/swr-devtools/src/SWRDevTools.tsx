b6093fde68d98f4afe1e524d16dba00f324ffd7a
Toru Kobayashi <koba0004@gmail.com>
2022-06-08 00:16:58 +0900

    fix: add window.__SWR_DEVTOOLS_REACT__ for demo apps

--- packages/swr-devtools/src/SWRDevTools.tsx
+++ packages/swr-devtools/src/SWRDevTools.tsx
@@ -8,6 +8,13 @@ export const SWRDevToolsContext = React.createContext<SWRDevToolsContextValue>({
   events: null,
 });
 
+// This is necessary until SWR has the DevTools hook.
+// https://github.com/vercel/swr/pull/2016
+if (typeof window !== "undefined") {
+  // @ts-expect-error
+  window.__SWR_DEVTOOLS_REACT__ = React;
+}
+
 export const SWRDevTools = ({ children }: { children: React.ReactNode }) => {
   const [swrdevtools, events] = useState(() => createSWRDevtools())[0];
   return (
