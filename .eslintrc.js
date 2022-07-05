64835ff73d999b47dca44a931215f42bc3211498
Toru Kobayashi <koba0004@gmail.com>
2022-07-06 00:29:12 +0900

    lint: suppress ESLint warnings (#62)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: "@cybozu/eslint-config/presets/react-typescript-prettier",
   rules: {
     "react/react-in-jsx-scope": "off",
+    "no-nested-ternary": "off",
+    "no-param-reassign": "off",
+    "consistent-return": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
   },
   env: {
     webextensions: true,
