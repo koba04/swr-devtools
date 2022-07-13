734820210150f21b9e7b6d985021f1fbefa63962
Toru Kobayashi <koba0004@gmail.com>
2022-07-13 10:19:02 +0900

    Revert "fix: a hydration error in SSR apps (#76)"
    This reverts commit c30626f44ec2eab1dabe81ee101aef4fbf19c2d2.

--- packages/swr-devtools-extensions/src/devtools.ts
+++ packages/swr-devtools-extensions/src/devtools.ts
@@ -1,16 +1,5 @@
-import { devtools, scripting } from "webextension-polyfill";
+import { devtools } from "webextension-polyfill";
 
 devtools.panels.create("SWR", "", "panel.html").then(() => {
   console.log("The DevTools panel has been created");
 });
-
-scripting
-  .executeScript({
-    target: {
-      tabId: devtools.inspectedWindow.tabId,
-      allFrames: true,
-    },
-    files: ["web-accessible.js"],
-  })
-  .then((r) => console.log("executeScript has been success", r))
-  .catch((e) => console.log("executeScript has been failed", e));
