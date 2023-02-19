d3a8eb0d3103a1dba293a8eb564883cf6a461f6c
Toru Kobayashi <koba0004@gmail.com>
2023-02-19 16:02:02 +0900

    chore(examples): add SWREntry & settings

--- examples/swr-devtools-demo/app/debug/page.tsx
+++ examples/swr-devtools-demo/app/debug/page.tsx
@@ -1,22 +1,33 @@
 "use client";
 import Head from "next/head";
 import Link from "next/link";
-import useSWR from "swr";
+import { useCallback, useState } from "react";
+import { SWRConfiguration } from "swr";
 
 import styles from "../../styles/infinite.module.css";
+import { SWREntry } from "./SWREntry";
 
-const sleep = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, Math.random() * 2000));
+type Settings = {
+  gridCount: number;
+};
 
-let count = 0;
-
-const fetcher = async (url) => {
-  await sleep(Math.random() * 2000);
-  return count++;
+type Options = SWRConfiguration & {
+  refreshInterval: number;
 };
 
 export default function Home() {
-  const { data, isLoading } = useSWR("/api/debug", fetcher);
+  const [settings, setSettings] = useState<Settings>({
+    gridCount: 10,
+  });
+  const [options, setOptions] = useState<Options>({
+    refreshInterval: 0,
+  });
+  const onChangeSettings = useCallback((setting: Partial<Settings>) => {
+    setSettings((current) => ({ ...current, ...setting }));
+  }, []);
+  const onChangeOptions = useCallback((option: Partial<Options>) => {
+    setOptions((current) => ({ ...current, ...option }));
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -25,10 +36,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1 className={styles.title}>Debug</h1>
+        <section>
+          <h2>Settings</h2>
+          <label>
+            Grid Count:&nbsp;
+            <input
+              type="text"
+              value={settings.gridCount}
+              onChange={(e) => {
+                onChangeSettings({ gridCount: +e.target.value });
+              }}
+            />
+          </label>
+        </section>
+        <section>
+          <h2>SWR Options</h2>
+          <label>
+            refreshInterval:&nbsp;
+            <input
+              type="text"
+              value={options.refreshInterval}
+              onChange={(e) => {
+                onChangeOptions({ refreshInterval: +e.target.value });
+              }}
+            />
+          </label>
+          <section />
+        </section>
         <section className={styles.content}>
-          <p>{isLoading ? "loading..." : data}</p>
+          <h2>Data</h2>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gap: 10,
+            }}
+          >
+            {Array.from({ length: settings.gridCount }).map((_, i) => {
+              const key = "test" + i;
+              return <SWREntry key={key} swrKey={key} options={options} />;
+            })}
+          </div>
         </section>
         <nav className={styles.nav}>
           <Link href="/">Go back</Link>
