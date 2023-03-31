0105a0e3919d3ae8ab9d652a69280f72a0d07494
Toru Kobayashi <koba0004@gmail.com>
2023-04-01 00:35:39 +1000

    chore: add @vercel/analytics

--- examples/swr-devtools-demo/pages/_app.tsx
+++ examples/swr-devtools-demo/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { SWRDevTools } from "swr-devtools";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SWRDevTools>
       <Component {...pageProps} />
+      <Analytics />
     </SWRDevTools>
   );
 }
